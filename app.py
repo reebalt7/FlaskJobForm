eda27906d25a4ce318268ff6f996c1c89fafc1b7
reebalt7 <swiftness753@gmail.com>
2024-08-15 15:51:18 -0600

    Final commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "myapplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "swiftness753@gmail.com"
+app.config["MAIL_PASSWORD"] = "nsqa jpwd aegf fnvi"
+
+db = SQLAlchemy(app)
+
+mail = Mail(app)
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(30))
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        date_object = datetime.strptime(date, "%Y-%m-%d")
+        occupation = request.form["occupation"]
+        print(first_name, last_name, email, date, occupation)
+
+        form = Form(first_name=first_name, last_name=last_name, email=email, date=date_object, occupation=occupation)
+
+        db.session.add(form)
+        db.session.commit()
+
+        message_body = f"Thank you for your submission {first_name}, we wil review it." \
+                       f"Here are your data: {first_name}, {last_name} submitted on {date}" \
+                       f"Thank you!"""
+        message = Message(subject="New form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
+
+        flash(f"{first_name}, Your form was submitted successfully!", "success")
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
